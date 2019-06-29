773451915349656f529043cf7085fdc485c81e19
sajrus33 <brianwala22@gmail.com>
2019-06-29 08:57:29 +0200

    start of the project

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const concat = require("gulp-concat");
+const cleanCSS = require("gulp-clean-css");
+const watch = require("gulp-watch");
+
+
+
+gulp.task("watch", function () {
+    watch(["./src/*.css"], () => {
+        return gulp.src(['./src/start.css', './src/cover.css', './src/works.css',
+            './src/about.css', './src/contact.css', './src/media.css'])
+            .pipe(concat("index.css"))
+            .pipe(cleanCSS())
+            .pipe(gulp.dest("./public/"));
+    });
+});
+
