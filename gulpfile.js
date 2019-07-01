7c63d66ba4ab97938bee10b51400050bdd707a05
sajrus33 <brianwala22@gmail.com>
2019-07-02 01:18:35 +0200

    skills 3/4

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,20 @@ const concat = require("gulp-concat");
 const cleanCSS = require("gulp-clean-css");
 const watch = require("gulp-watch");
 
-
-
-gulp.task("watch", function () {
-    watch(["./src/*.css"], () => {
-        return gulp.src(['./src/start.css', './src/cover.css', './src/works.css',
-            './src/about.css', './src/contact.css', './src/media.css'])
-            .pipe(concat("index.css"))
-            .pipe(cleanCSS())
-            .pipe(gulp.dest("./public/"));
-    });
+gulp.task("watch", function() {
+  watch(["./src/*.css"], () => {
+    return gulp
+      .src([
+        "./src/start.css",
+        "./src/cover.css",
+        "./src/works.css",
+        "./src/about.css",
+        "./src/skills.css",
+        "./src/contact.css",
+        "./src/media.css"
+      ])
+      .pipe(concat("index.css"))
+      .pipe(cleanCSS())
+      .pipe(gulp.dest("./public/"));
+  });
 });
-
